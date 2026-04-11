880dae030fea2112b6012640afdfbc05064387dd
i.repnev <iljusha1@gmail.com>
2026-04-11 21:10:36 +0300

    mac clang fix

--- include/unet/http/upgrade_context.hpp
+++ include/unet/http/upgrade_context.hpp
@@ -5,16 +5,23 @@
 #include "unet/http/session.hpp"
 
 namespace usub::unet::http {
+    #if defined(__cpp_lib_move_only_function) && __cpp_lib_move_only_function >= 202110L
+    template<typename Signature>
+    using upgrade_session_factory = std::move_only_function<Signature>;
+    #else
+    template<typename Signature>
+    using upgrade_session_factory = std::function<Signature>;
+    #endif
 
     // Passed by ref to upgrade route handlers. The handler calls ctx.accept() to signal
     // a protocol upgrade, or leaves it untouched to fall back to normal HTTP (Response& is sent).
     struct UpgradeContext {
         bool accepted{false};
-        std::move_only_function<SessionBox()> make_session;
+        upgrade_session_factory<SessionBox()> make_session;
 
         // Signal upgrade. Server will send the handler's Response& as the wire handshake,
         // then transition the connection to the session produced by factory().
-        void accept(std::move_only_function<SessionBox()> factory) {
+        void accept(upgrade_session_factory<SessionBox()> factory) {
             accepted   = true;
             make_session = std::move(factory);
         }
